ce1ee4b58362a06c3b006803c7da52f952f807e7
kobitoko <kobitoko@gmail.com>
2015-10-29 13:04:43 -0600

    Initial Game Model class with getters and setters.

--- app/src/androidTest/java/ca/ualberta/t14/gametrader/GameTest.java
+++ app/src/androidTest/java/ca/ualberta/t14/gametrader/GameTest.java
@@ -9,14 +9,18 @@ public class GameTest extends ActivityInstrumentationTestCase2 {
     }
 
     public void testGame() {
+        // Create items that get added to your inventory (1.1 AddItemToInventory)
+        // This is testing the model, not the controller.
         Game item = new Game();
         item.setPlatform(Game.Platform.PLAYSTATION1);
         item.setTitle("Crash Bandicoot Team Racing");
         item.setCondition(Game.Condition.NEW);
+        // Being able to flag an item as not listed (1.4 FlagItemAsNotListed)
         item.setSharable(Boolean.TRUE);
-        item.setAdditionalInfo("Still in original package!");
+        item.setAdditionalInfo("Still in original package! You get the honour of opening it.");
         item.setQuantities(1);
 
+        // Including attaching a photo (4.1 AttachPhotographToItem)
         Image pic = new Image();
         item.addPhoto(pic);
     }
