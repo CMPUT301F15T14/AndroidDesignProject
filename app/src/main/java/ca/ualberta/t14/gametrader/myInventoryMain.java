25ef0f64dbad293600feefe0949fcfe7c4b67a07
bcvancouver <michael-xi@hotmail.com>
2015-10-29 19:12:58 -0600

    my inventory main layout added

---
+++ app/src/main/java/ca/ualberta/t14/gametrader/myInventoryMain.java
@@ -0,0 +1,14 @@
+package ca.ualberta.t14.gametrader;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class myInventoryMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_inventory_main);
+    }
+
+}
