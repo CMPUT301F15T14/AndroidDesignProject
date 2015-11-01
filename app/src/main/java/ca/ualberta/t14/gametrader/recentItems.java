5cde4645e7ec9948a8b9659246f433d8d0c8e572
bcvancouver <michael-xi@hotmail.com>
2015-11-01 14:33:35 -0700

    commit

--- app/src/main/java/ca/ualberta/t14/gametrader/recentItems.java
+++ app/src/main/java/ca/ualberta/t14/gametrader/recentItems.java
@@ -1,14 +1,13 @@
 package ca.ualberta.t14.gametrader;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 
-public class myInventoryMain extends Activity {
+public class recentItems extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_inventory_main);
+        setContentView(R.layout.activity_recent_items);
     }
-
 }
