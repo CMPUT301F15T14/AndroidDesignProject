f0fd9f483b62c3479da8ef78b1add591ab773e78
jjohnsto <jjohnsto@ualberta.ca>
2015-11-01 13:32:47 -0700

    Forgot to add new files.

---
+++ app/src/androidTest/java/ca/ualberta/t14/gametrader/UserTest.java
@@ -0,0 +1,16 @@
+package ca.ualberta.t14.gametrader;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by jjohnsto on 11/1/15.
+ */
+public class UserTest extends ActivityInstrumentationTestCase2 {
+    public UserTest() {
+        super(MainActivity.class);
+    }
+
+    public void testEditProfile() {
+
+    }
+}
