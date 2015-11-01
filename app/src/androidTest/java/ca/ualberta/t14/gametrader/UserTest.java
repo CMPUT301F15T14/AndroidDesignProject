8d4c1949ad174de393f3439d6a502e37e9c575d3
jjohnsto <jjohnsto@ualberta.ca>
2015-11-01 15:28:18 -0700

    worked on user tests.

--- app/src/androidTest/java/ca/ualberta/t14/gametrader/UserTest.java
+++ app/src/androidTest/java/ca/ualberta/t14/gametrader/UserTest.java
@@ -1,16 +1,20 @@
 package ca.ualberta.t14.gametrader;
 
+import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 
 /**
  * Created by jjohnsto on 11/1/15.
  */
 public class UserTest extends ActivityInstrumentationTestCase2 {
+
     public UserTest() {
         super(MainActivity.class);
     }
 
     public void testEditProfile() {
+        EditProfileActivity activity = (EditProfileActivity) getActivity();
 
+        activity.
     }
 }
