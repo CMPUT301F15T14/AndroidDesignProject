19fed0f980b2527419d1edc0d460709db5076f26
kobitoko <kobitoko@gmail.com>
2015-11-01 16:45:13 -0700

    Thinking about GameController: wip

---
+++ app/src/androidTest/java/ca/ualberta/t14/gametrader/GameControllerTest.java
@@ -0,0 +1,27 @@
+package ca.ualberta.t14.gametrader;
+
+/**
+ * Created by satyabra on 11/1/15.
+ */
+public class GameControllerTest {
+
+    public GameControllerTest() {}
+
+    public void testController() {
+    /*
+    UML stuff +
+     Create items that get added to your inventory (1.1), including attaching a photo (4.1) and being able to flag an item as not listed (1.4)
+     RE: 4.1 for controller: http://stackoverflow.com/questions/15662258/how-to-save-a-bitmap-on-internal-storage
+      the controller will actually get the location of the image. For possible test, create empty image like before, then save it on disk, get path and test put path.
+      for testing use method File getAbsoluteFile ()
+      after test, clean up your stuff using delete that file:  (File delete() method)
+      http://developer.android.com/reference/java/io/File.html#delete%28%29
+
+    */
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/ca/ualberta/t14/gametrader/GameController.java
@@ -0,0 +1,7 @@
+package ca.ualberta.t14.gametrader;
+
+/**
+ * Created by satyabra on 11/1/15.
+ */
+public class GameController {
+}
