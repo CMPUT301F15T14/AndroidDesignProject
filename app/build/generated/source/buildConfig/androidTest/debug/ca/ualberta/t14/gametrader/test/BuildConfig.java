a53ccb0a17c141c9691f2d6a020cd33157102abf
kobitoko <kobitoko@gmail.com>
2015-11-17 15:20:24 -0700

    updated JavDoc in gh-pages branch

---
+++ app/build/generated/source/buildConfig/androidTest/debug/ca/ualberta/t14/gametrader/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ca.ualberta.t14.gametrader.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ca.ualberta.t14.gametrader.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
