5b8199afe0a630c616ea91cbf49cea94e1631199
kobitoko <kobitoko@gmail.com>
2015-11-17 15:10:47 -0700

    wip Picture Manager

---
+++ app/src/main/java/ca/ualberta/t14/gametrader/PictureManager.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2015  Aaron Arnason, Tianyu Hu, Michael Xi, Ryan Satyabrata, Joel Johnston, Suzanne Boulet, Ng Yuen Tung(Brigitte)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package ca.ualberta.t14.gametrader;
+
+/**
+ * This is a manager for photos, the images will actually be stored in the elastic search online.
+ * This Model will store the local JSON string of the images and downloaded JSON strings of images online.
+ *
+ * @author  Ryan Satyabrata
+ */
+public class PictureManager {
+}
