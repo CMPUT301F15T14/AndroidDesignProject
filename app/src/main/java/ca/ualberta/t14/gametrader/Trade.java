17abaae4ae4aa6c1301779a90b449d3d0c38e793
Belzet <sboulet@ualberta.ca>
2015-11-19 12:15:08 -0700

    Added Trade functionality and documentation

--- app/src/main/java/ca/ualberta/t14/gametrader/Trade.java
+++ app/src/main/java/ca/ualberta/t14/gametrader/Trade.java
@@ -18,8 +18,189 @@
 
 package ca.ualberta.t14.gametrader;
 
+import java.util.ArrayList;
+
 /**
- * Created by sboulet on 11/19/15.
+ * This class stores information on a trade between two users:
+ * the borrower, who initiates the trade, and the
+ * owner, who the borrower wishes to trade with.
+ * The borrower can ask for 1 to many games from the owner's inventory
+ * and can offer 0 to many games in return.
+ *
+ * The owner can also offer a countertrade back to the borrower.
+ *
+ * @author Suzanne Boulet
  */
-public class Trade {
+public class Trade extends FileIO implements AppObservable{
+
+    private volatile ArrayList<AppObserver> observers;
+
+    @Override
+    public void addObserver(AppObserver observer) {
+        observers.add(observer);
+    }
+
+    private void notifyAllObservers() {
+        for(AppObserver obs : observers) {
+            obs.appNotify(this);
+        }
+    }
+
+    /**
+     * An enumeration of the status of the trade.
+     * OWNERAPPROVAL: when the trade is first offered to the owner,
+     * or when the borrower sends a countertrade
+     * BORROWERAPPOVAL: when the owner countertrades
+     * COMPLETE: when a trade is accepted on either end
+     */
+    public enum TradeStatus {OWNERAPPROVAL,BORROWERAPPROVAL,COMPLETE}
+
+    private User borrower;
+    private ArrayList<Game> borrowerOffers;
+    private User owner;
+    private ArrayList<Game> ownerOffers;
+    private int tradeId;
+    private String ownersComment;
+    private TradeStatus status;
+
+    /**
+     * creates a trade using a Game passed by the activity, adding it to ownerOffers.
+     *
+     * Initializes the trade. Creates an empty ArrayList for borrowerOffers,
+     * initializes borrower based on user id and owner based on game owner's id
+     * creates a blank comment for trade and set tradeStatus to OWNERAPPROVAL
+     * initializes an empty list of observers
+     * and randomly generates a tradeId.
+     *
+     * @param game
+     */
+    public Trade(Game game) {
+        borrowerOffers = new ArrayList<Game>();
+        //borrower = current user
+        //owner = user with id
+        ownerOffers = new ArrayList<Game>();
+        ownerOffers.add(game);
+        ownersComment = "";
+        status = TradeStatus.OWNERAPPROVAL;
+        observers = new ArrayList<AppObserver>();
+        //tradeId = randomly generated number?
+    }
+
+    /**
+     * A getter of the creator of the trade.
+     *
+     * @return User who first offered the trade (borrower)
+     */
+    public User getBorrower() {
+        return borrower;
+    }
+
+    /**
+     * A setter to set a passed User as the borrower (initiator) of the trade
+     * The borrower will be set in initialization, so only to be used if an error occured.
+     *
+     * @param borrower
+     */
+    public void setBorrower(User borrower) {
+        this.borrower = borrower;
+        notifyAllObservers();
+    }
+
+    /**
+     * A getter that returns the list of games offered by the borrower in the trade
+     *
+     * @return ArrayList of the Games offered by the borrower (borrowerOffers)
+     */
+    public ArrayList<Game> getBorrowerOffers() {
+        return borrowerOffers;
+    }
+
+    /**
+     * A setter that adds the passed Game to the ArrayList borrowerOffers
+     *
+     * @param game
+     */
+    public void addBorrowerGame(Game game) {
+        borrowerOffers.add(game);
+        notifyAllObservers();
+    }
+
+    /**
+     * A getter of the receiver of the trade offer
+     *
+     * @return the User who gets offered the trade (owner)
+     */
+    public User getOwner() {
+        return owner;
+    }
+
+    /**
+     * A setter that takes the passed User and sets it as owner
+     * The owner will be set in initialization, so only to be used if an error occured.
+     *
+     * @param owner
+     */
+    public void setOwner(User owner) {
+        this.owner = owner;
+        notifyAllObservers();
+    }
+
+    /**
+     * A getter that returns the list of games offered by the owner in the trade
+     *
+     * @return the ArrayList of Games offered by the owner (ownerOffers)
+     */
+    public ArrayList<Game> getOwnerOffers() {
+        return ownerOffers;
+    }
+
+    /**
+     * A setter that adds the passed Game to the ArrayList ownerOffers
+     *
+     * @param game
+     */
+    public void addOwnerGame(Game game) {
+        ownerOffers.add(game);
+        notifyAllObservers();
+    }
+
+    /**
+     * A getter that returns the comment attached to the trade.
+     *
+     * @return String ownerComment
+     */
+    public String getOwnersComment() {
+        return ownersComment;
+    }
+
+    /**
+     * A setter that takes String comment and sets it as ownersComment
+     *
+     * @param comment
+     */
+    public void setOwnersComment(String comment) {
+        ownersComment = comment;
+        notifyAllObservers();
+    }
+
+    /**
+     * The tag of the status of the trade
+     * Used to classify who has the choice to accept the trade or not
+     *
+     * @return enumeration of the trade's status
+     */
+    public TradeStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * The tag of the status of the trade
+     * Used to change which user has the choice to accept the trade
+     *
+     * @param status
+     */
+    public void setStatus(TradeStatus status) {
+        this.status = status;
+        notifyAllObservers();
+    }
 }
