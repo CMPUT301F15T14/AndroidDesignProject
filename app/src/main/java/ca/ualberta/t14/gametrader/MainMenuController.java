8fc40ccf3f1c1729845ae62d74a818ac8d3c8bfb
kobitoko <kobitoko@gmail.com>
2015-11-27 14:40:13 -0700

    loading apps json is asynchronous. Hence SPEEED launch

--- app/src/main/java/ca/ualberta/t14/gametrader/MainMenuController.java
+++ app/src/main/java/ca/ualberta/t14/gametrader/MainMenuController.java
@@ -13,32 +13,43 @@ public class MainMenuController {
     SettingsMode settingsMode = new SettingsMode();
 
     void preLoadAllSingletons(Context context) {
+        final Context c = context;
 
-        // Try setting the user singleton to the loaded user.
-        try {
-            user = (User) user.loadJson("MainUserProfile", context);
-            UserSingleton.getInstance().setUser(user);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Inside a runnable because put in a new thread, it won't freeze the UI that way.
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
 
-        // Try to load the user's settings.
-        try {
-            settingsMode = (SettingsMode) settingsMode.loadJson(SettingsMode.SETTINGS_FILE, context);
-            SettingsSingleton.getInstance().setSettings(settingsMode);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+                // Try setting the user singleton to the loaded user.
+                try {
+                    user = (User) user.loadJson("MainUserProfile", c);
+                    UserSingleton.getInstance().setUser(user);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-        // Check if the unique installation identity exists, and add one if there is none.
-        if (UserSingleton.getInstance().getUser().getInstallationId() == null) {
-            // Retrieve/Create unique installation identity.
-            InstallationIdGenerator iIdG = new InstallationIdGenerator();
-            String installationIdStr = iIdG.id(context);
+                // Try to load the user's settings.
+                try {
+                    settingsMode = (SettingsMode) settingsMode.loadJson(SettingsMode.SETTINGS_FILE, c);
+                    SettingsSingleton.getInstance().setSettings(settingsMode);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-            // Set the unique installation identity if user has none.
-            UserSingleton.getInstance().getUser().setInstallationId(installationIdStr);
-        }
+                // Check if the unique installation identity exists, and add one if there is none.
+                if (UserSingleton.getInstance().getUser().getInstallationId() == null) {
+                    // Retrieve/Create unique installation identity.
+                    InstallationIdGenerator iIdG = new InstallationIdGenerator();
+                    String installationIdStr = iIdG.id(c);
+
+                    // Set the unique installation identity if user has none.
+                    UserSingleton.getInstance().getUser().setInstallationId(installationIdStr);
+                }
+            }
+        };
+
+        Thread t = new Thread(r);
+        t.start();
     }
 
 }
