55b2aeb9f997a2c7077bf33dfd09037a7d6fa596
bcvancouver <michael-xi@hotmail.com>
2015-11-30 14:48:50 -0700

    move some test to InventoryActivityTest

--- app/src/androidTest/java/ca/ualberta/t14/gametrader/InventoryActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/t14/gametrader/InventoryActivityTest.java
@@ -1,7 +1,6 @@
 package ca.ualberta.t14.gametrader;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.app.Instrumentation;
 import android.content.DialogInterface;
 import android.test.ActivityInstrumentationTestCase2;
@@ -10,28 +9,44 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 
-import java.util.ArrayList;
-
 /**
- * Created by michaelximac on 2015-11-01.
+ * Created by michaelximac on 2015-11-30.
  */
-public class InventoryTest extends ActivityInstrumentationTestCase2{
-    public InventoryTest() {
-        super(ca.ualberta.t14.gametrader.InventoryListActivity.class);
+public class InventoryActivityTest extends ActivityInstrumentationTestCase2 {
+    public InventoryActivityTest(){
+        super(ca.ualberta.t14.gametrader.MainActivity.class);
     }
 
     public void testViewInventory(){
-        InventoryListActivity listActivity= (InventoryListActivity) getActivity();
-
-        final ListView GameList=listActivity.getGameList();
-        /*listActivity.runOnUiThread(new Runnable() {
+        MainActivity MainMenu=(MainActivity)getActivity();
+        final Button inventory=MainMenu.getInventoryButton();
+        // Set up a new ActivityMonitor
+        Instrumentation.ActivityMonitor listActivityMonitor =
+                getInstrumentation().addMonitor(InventoryListActivity.class.getName(),
+                        null, false);
+        MainMenu.runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 User testUser=new User();
                 UserSingleton.getInstance().setUser(testUser);
                 testUser.getInventory().clear();
+                inventory.performClick();
             }
-        });*/
+        });
+        getInstrumentation().waitForIdleSync();
+
+        // Validate that ReceiverActivity is started
+        InventoryListActivity listActivity = (InventoryListActivity)
+                listActivityMonitor.waitForActivityWithTimeout(1000);
+        assertNotNull("ReceiverActivity is null", listActivity);
+        assertEquals("Monitor for ReceiverActivity has not been called",
+                1, listActivityMonitor.getHits());
+        assertEquals("Activity is of wrong type",
+                InventoryListActivity.class, listActivity.getClass());
+        //InventoryListActivity listActivity= (InventoryListActivity) getActivity();
+
+        final ListView GameList=listActivity.getGameList();
+
         addGameTest(listActivity, GameList);
         // Set up a new ActivityMonitor
         Instrumentation.ActivityMonitor inventoryItemMonitor =
@@ -119,6 +134,8 @@ public class InventoryTest extends ActivityInstrumentationTestCase2{
                 1, inventoryItemMonitor.getHits());
         assertEquals("Activity is of wrong type",
                 InventoryItemActivity.class, inventoryItem.getClass());
+        assertEquals("GTA V", inventoryItem.getGameTitle());
+        assertEquals("PC", inventoryItem.getPlatform());
 
         return inventoryItem;
     }
@@ -171,43 +188,5 @@ public class InventoryTest extends ActivityInstrumentationTestCase2{
         assertEquals(expectedCount, actualCount);
         // Remove the ActivityMonitor
         getInstrumentation().removeMonitor(deleteItemMonitor);
-    }
-
-    public void testSearch() {
-        Inventory testInv = new Inventory();
-        InventoryController testInvCnt = new InventoryController(testInv);
-
-        Game g1 = new Game("Halo: Combat Evolved");
-        g1.setPlatform(Game.Platform.PC);
-        Game g2 = new Game("Halo 2");
-        g2.setPlatform(Game.Platform.XBOX);
-        Game g3 = new Game("Halo 3");
-        g3.setPlatform(Game.Platform.XBOX360);
-        Game g4 = new Game("Teletubies Invade Earth");
-        g3.setPlatform(Game.Platform.PLAYSTATION1);
-
-        ArrayList<Game> result = testInvCnt.Search("Halo");
-        assertTrue(result.isEmpty());
-
-        testInvCnt.addItem(g1);
-        testInvCnt.addItem(g2);
-        testInvCnt.addItem(g3);
-        testInvCnt.addItem(g4);
-
-        ArrayList<Game> expectedResult = new ArrayList<Game>();
-        expectedResult.add(g1);
-        expectedResult.add(g2);
-        expectedResult.add(g3);
-
-        result = testInvCnt.Search("Halo");
-
-        assertTrue(result.containsAll(expectedResult));
-        assertFalse(result.contains(g4));
-
-        result = testInvCnt.Search("Halo", Game.Platform.PC);
-        assertTrue(result.contains(g1));
-        assertFalse(result.contains(g2));
-        assertFalse(result.contains(g3));
-        assertFalse(result.contains(g4));
     }
 }
